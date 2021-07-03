007828d99d750a575488b4d4ee2339252ff8d124
ll-leo <68234660+ll-leo@users.noreply.github.com>
2021-07-03 08:52:27 +0800

    add file
    no

---
+++ 13_uart/project/main.c
@@ -0,0 +1,42 @@
+#include "bsp_clk.h"
+#include "bsp_delay.h"
+#include "bsp_led.h"
+#include "bsp_beep.h"
+#include "bsp_key.h"
+#include "bsp_int.h"
+#include "bsp_uart.h"
+#include "stdio.h"
+
+/*
+ * @description	: main函数
+ * @param 		: 无
+ * @return 		: 无
+ */
+int main(void)
+{
+	int a=0;
+	int b=0;
+	
+
+	int_init(); 				/* 初始化中断(一定要最先调用！) */
+	imx6u_clkinit();			/* 初始化系统时钟 			*/
+	delay_init();				/* 初始化延时 			*/
+	clk_enable();				/* 使能所有的时钟 			*/
+	led_init();					/* 初始化led 			*/
+	beep_init();				/* 初始化beep	 		*/
+	uart_init();				/* 初始化串口，波特率115200 */
+
+	while(1)				
+	{	
+		printf("please put the first num");
+		scanf("%d",&a);
+		printf("\n");
+
+		printf("please put the second num");
+		scanf("%d",&b);
+		printf("\n");
+
+		printf("num1=%d+num2=%d == %d",a,b,a+b);
+	}
+	return 0;
+}
